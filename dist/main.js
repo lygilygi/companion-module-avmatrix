b1c34c8a4a238d0c810401822a4b0a99cbc74ac5
Pawel Lygan <pawel@pawels-macbook-pro.tail63b59b.ts.net>
2025-12-31 13:57:16 +0100

    chore: stop tracking dist output

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = require("@companion-module/base");
-const instance_1 = require("./instance");
-(0, base_1.runEntrypoint)(instance_1.AVMatrixInstance, []);
